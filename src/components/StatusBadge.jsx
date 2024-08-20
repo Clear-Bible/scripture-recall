90fa56f468de4c58adebabc695ad7d50047def04
Mike Brinker <themikejr@gmail.com>
2024-08-20 09:51:14 -0400

    Add simple snippet card to voice chat UI.

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -1,15 +1,14 @@
 import { Badge } from "@/components/ui/badge";
 
- const statusColors = {
-    "3": "bg-green-500",
-    "2": "bg-yellow-500",
-    "1": "bg-red-500",
-  };
+const statusColors = {
+  3: "bg-green-500",
+  2: "bg-yellow-500",
+  1: "bg-red-500",
+};
 
-function StatusBadge({status}) {
-// console.info("BADGE", status)
-return (<Badge className={`${statusColors[status]}`}> &nbsp; </Badge>);
-
+function StatusBadge({ status }) {
+  // console.info("BADGE", status)
+  return <Badge className={`${statusColors[status]} text-xs`}> &nbsp; </Badge>;
 }
 
 export default StatusBadge;
--- src/components/VoiceChat.jsx
+++ src/components/VoiceChat.jsx
@@ -1,27 +1,29 @@
 import { VoiceProvider } from "@humeai/voice-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import Messages from "@/components/voice/Messages";
 import Controls from "@/components/voice/Controls";
 import StartCall from "@/components/voice/StartCall";
 import { useRef, useState, useEffect } from "react";
-import {useParams} from "react-router-dom";
-import {Speech} from "lucide-react";
+import { useParams } from "react-router-dom";
+import { Speech } from "lucide-react";
 
 import { AnimatePresence, motion } from "framer-motion";
 
-import {createPrompt} from "@/data/prompts";
+import StatusBadge from "@/components/StatusBadge";
+import { createPrompt } from "@/data/prompts";
 import { getSnippetById } from "@/db";
 
 export function VoiceChat({ accessToken }) {
   const timeout = useRef(null);
   const ref = useRef(null);
-  const {snippetId} = useParams()
+  const { snippetId } = useParams();
 
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   const [snippet, setSnippet] = useState(null);
 
   console.info("VOICE CHAT: ", snippetId, snippet);
-
 
   useEffect(() => {
     const fetchSnippet = async () => {
@@ -38,7 +40,7 @@ export function VoiceChat({ accessToken }) {
     fetchSnippet();
   }, []);
 
- if (isLoading) {
+  if (isLoading) {
     return (
       <AnimatePresence>
         <motion.div
@@ -60,63 +62,76 @@ export function VoiceChat({ accessToken }) {
     );
   }
 
-
-if (snippet) {
-return (
-    <div
-      className={"relative grow flex flex-col mx-auto w-full overflow-hidden"}
-    >
-      <VoiceProvider
-        debug={true}
-        auth={{ type: "apiKey", value: import.meta.env.VITE_HUME_API_KEY }}
-        // auth={{ type: "accessToken", value: accessToken }}
-        sessionSettings={{
-          systemPrompt: createPrompt(snippet)
-        }}
-        onMessage={() => {
-          if (timeout.current) {
-            window.clearTimeout(timeout.current);
-          }
-
-          timeout.current = window.setTimeout(() => {
-            if (ref.current) {
-              const scrollHeight = ref.current.scrollHeight;
-
-              ref.current.scrollTo({
-                top: scrollHeight,
-                behavior: "smooth",
-              });
-            }
-          }, 200);
-        }}
+  if (snippet) {
+    return (
+      <div
+        className={"relative grow flex flex-col mx-auto w-full overflow-hidden"}
       >
-          <AnimatePresence>
-          <motion.div 
-            className="fixed inset-0 flex items-center justify-center"
-          initial="initial"
-          animate="enter"
-          exit="exit"
-          variants={{
-            initial: { opacity: 0 },
-            enter: { opacity: 1 },
-            exit: { opacity: 0 },
+        <div className="p-4">
+          <Card key={snippet.id}>
+            <CardHeader>
+              <CardTitle className="text-md">
+                <div className="flex justify-between items-center mb-2">
+                  <div>{snippet.reference}</div>
+                  <div>
+                    <StatusBadge status={snippet.status} />
+                  </div>
+                </div>
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <VoiceProvider
+          debug={true}
+          auth={{ type: "apiKey", value: import.meta.env.VITE_HUME_API_KEY }}
+          // auth={{ type: "accessToken", value: accessToken }}
+          sessionSettings={{
+            systemPrompt: createPrompt(snippet),
+          }}
+          onMessage={() => {
+            if (timeout.current) {
+              window.clearTimeout(timeout.current);
+            }
+
+            timeout.current = window.setTimeout(() => {
+              if (ref.current) {
+                const scrollHeight = ref.current.scrollHeight;
+
+                ref.current.scrollTo({
+                  top: scrollHeight,
+                  behavior: "smooth",
+                });
+              }
+            }, 200);
           }}
         >
-
-          <Speech className="size-36 opacity-50" 
-            strokeWidth={2}
-            stroke={"currentColor"}
-            />
+          <AnimatePresence>
+            <motion.div
+              className="fixed inset-0 flex items-center justify-center"
+              initial="initial"
+              animate="enter"
+              exit="exit"
+              variants={{
+                initial: { opacity: 0 },
+                enter: { opacity: 1 },
+                exit: { opacity: 0 },
+              }}
+            >
+              <Speech
+                className="size-36 opacity-50"
+                strokeWidth={2}
+                stroke={"currentColor"}
+              />
             </motion.div>
-            </AnimatePresence>
-        {/* <Messages ref={ref} /> */}
-        <Controls />
-        <StartCall />
-      </VoiceProvider>
-    </div>
-  );
-
+          </AnimatePresence>
+          {/* <Messages ref={ref} /> */}
+          <Controls />
+          <StartCall />
+        </VoiceProvider>
+      </div>
+    );
   }
-  }
+}
 
 export default VoiceChat;

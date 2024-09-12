81bbd1f9e511975e625aebb3df6aee769eccc1a6
Mike Brinker <themikejr@gmail.com>
2024-09-12 14:04:14 -0400

    Test bcv parser fork.
    see: https://github.com/openbibleinfo/Bible-Passage-Reference-Parser/issues/53

--- src/components/text/Chat.jsx
+++ src/components/text/Chat.jsx
@@ -15,7 +15,9 @@ import { saveSnippet } from "@/db/snippets";
 import MemoryVerseDialog from "../memory/MemoryVerseDialog";
 import { getVersesByReference } from "@/db/bible";
 
-import * as bcvLib from "bible-passage-reference-parser/js/en_bcv_parser";
+// import * as bcvLib from "bible-passage-reference-parser/js/en_bcv_parser";
+import * as parserModule from "bible-ref-parse/js/en_bcv_parser";
+const parser = new parserModule.bcv_parser();
 
 const Chat = ({ mode, snippet, initialPrompt }) => {
   const UserMessage = ({ text }) => {
@@ -55,7 +57,7 @@ const Chat = ({ mode, snippet, initialPrompt }) => {
 
     useEffect(() => {
       async function addVerseBodies() {
-        const bcv = new bcvLib.bcv_parser();
+        const bcv = parser;
 
         bcv.set_options({
           consecutive_combination_strategy: "separate",
